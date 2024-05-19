656937df944d50f0f5ee119f49e348ae486efd8a
Grenyerre <robertgrenyer@btinternet.com>
2024-05-19 16:28:16 +0100

    Add code for construction of 3 discrete board sizes

--- run.py
+++ run.py
@@ -9,20 +9,48 @@ Legend:
 """
 from random import randint
 
-def easy_game(board):
-    for ship in range(5):
-        ship_row, ship_column = randint(0,3), randint(0,3)
-        while board[ship_row][ship_column] == "X":
-            ship_row, ship_column = get_ship_location()
-        board[ship_row][ship_column] = "X"
-    print_board(board):
+# Construct the game board
+e_hidden_board = [[''] * 4 for x in range(4)]
+e_guess_board = [[''] * 4 for x in range(4)]
+m_hidden_board = [[''] * 6 for x in range(6)]
+m_guess_board = [[''] * 6 for x in range(6)]
+h_hidden_board = [[''] * 8 for x in range(8)]
+h_guess_board = [[''] * 8 for x in range(8)]
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7} 
+
+
+
+def print_board_easy(board):
     print('   A B C D')
     print('------------')
     row_number = 1
     for row in board:
         print(" %d|%s |" % (row_number, " |".join(row)))
         row_number += 1
+    
+def create_ships_easy(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,3), randint(0,3)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = get_ship_location_easy()
+        board[ship_row][ship_column] = "X"
+    
+def get_ship_location_easy():
+    row = input('Please enter a row number for your shot, between 1 and 4: ')
+    while row not in '1234':
+        print('Number outside of game area, please enter a valid row number: ')
+        row = input('Please enter a row number for your shot, between 1 and 4: ')
+    
+    column = input('Please enter a column letter for your shot, between A and D: ').upper()
+    while column not in 'ABCD':
+        print('Number outside of game area, please enter a valid column letter: ')
+        column = input('Please enter a column letter for your shot, between A and D: ').upper()
+    
+    return int(row) - 1, letters_to_numbers[column]
 
+
+"""
 def medium_game(board):
     for ship in range(5):
         ship_row, ship_column = randint(0,5), randint(0,5)
@@ -50,32 +78,28 @@ def hard_game(board):
     for row in board:
         print(" %d|%s |" % (row_number, " |".join(row)))
         row_number += 1
-
+"""
+        
 # Select game difficulty level
 level = input('Please select a level of difficulty: E (easy), M (medium), H (hard): ').lower()
-while level not in ['E', 'M', 'H']:
-    print('Invalid choice, please select a valid level of difficulty')
-    level = input('Please select a level of difficulty: E (easy), M (medium), H (hard): ').lower()
-    if level == 'E':
-        def easy_game(board):
-    elif level == 'M':
-        def medium_game(board):
-    else:
-        def hard_game(board):
+if level == 'E':
+    create_ships_easy(e_hidden_board)
+    print_board_easy(e_guess_board)
+    get_ship_location_easy()
+elif level == 'M':
+    #create_ships_medium(m_hidden_board)
+    #print_board_medium(m_guess_board)
+    #get_ship_location_medium()
+else:
+    #create_ships_hard(h_hidden_board)
+    #print_board_hard(h_guess_board)
+    #get_ship_location_hard()
+while level not in 'EMH':
+        print('Invalid choice, please select a valid level of difficulty')
+        level = input('Please select a level of difficulty: E (easy), M (medium), H (hard): ').lower()
+    
+        
 
-
-letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}       
-
-def get_ship_location():
-    row = input('Please enter a row number for your shot, between 1 and 8: ')
-    while row not in '12345678':
-        print('Number outside of game area, please enter a valid row number: ')
-        row = input('Please enter a row number for your shot, between 1 and 8: ')
-    column = input('Please enter a column letter for your shot, between A and H: ').upper()
-    while column not in 'ABCDEFGH':
-        print('Number outside of game area, please enter a valid column letter: ')
-        column = input('Please enter a column letter for your shot, between A and H: ').upper()
-    return int(row) - 1, letters_to_numbers[column]    
 
 def count_hit_ships(board):
     count = 0
@@ -84,19 +108,17 @@ def count_hit_ships(board):
             if column == "X":
                 count += 1
     return count
-    
 
-create_ships(e_hidden_board, m_hidden_board, h_hidden_board)
 
 turns = 10
 while turns > 0:
     print('\nWelcome to Battleship')
     print('---------------------- ')
-    print_board()
-    row, column = get_ship_location()
-    if e_guess_board, m_guess_board, h_guess_board[row][column] == "-":
+    print_board_easy()
+    row, column = get_ship_location_easy()
+    if e_guess_board[row][column] == "-":
         print('You already guessed that location, it was a miss! Try again')
-    elif e_hidden_board, m_hidden_board, h_hidden_board[row][column] == "X":
+    elif e_hidden_board[row][column] == "X":
         print('Good shot! You hit a battleship')
         e_guess_board, m_guess_board, h_guess_board[row][column] = "X"
         turns -= 1
