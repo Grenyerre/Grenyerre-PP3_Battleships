188b13148f19243424beda344181f998f975c22c
Grenyerre <robertgrenyer@btinternet.com>
2024-05-21 12:04:27 +0100

    Add function headings and parameters

--- run.py
+++ run.py
@@ -19,3 +19,17 @@ Dictionary to convert letters to numbers allows user to use
 Integers for row and column input
 """
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+
+def print_board(board):
+
+
+def create_ships(board):
+
+
+def get_ship_location():
+
+
+def count_hit_ships(board):
+
+
