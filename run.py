ffa71dcb97b6a36793613d09c13e05e5304edd0f
Grenyerre <robertgrenyer@btinternet.com>
2024-05-21 13:42:28 +0100

    Add main game while loop

--- run.py
+++ run.py
@@ -71,4 +71,28 @@ create_ships(hidden_board)
 
 # Set the number of turns to 10
 turns = 10
+# While loop to run the game until the user has no turns left
+while turns > 0:
+    print('\nWelcome to Battleship')
+    print('---------------------- ')
+    print_board(hidden_board)
+    print_board(guess_board)
+    row, column = get_ship_location()
+    if guess_board[row][column] == "-":
+        print('You already guessed that location, it was a miss! Try again')
+    elif hidden_board[row][column] == "X":
+        print('Good shot! You hit a battleship')
+        guess_board[row][column] = "X"
+        turns -= 1
+    else:
+        print('You missed!')
+        guess_board[row][column] = "-"
+        turns -= 1
+    if count_hit_ships(guess_board) == 5:
+        print('Congratulations! You have sunk all the battleships')
+        break
+    print('You have ' + str(turns) + ' turns remaining')
+    if turns == 0:
+        print('Game Over! You have run out of turns')
+        break
 
