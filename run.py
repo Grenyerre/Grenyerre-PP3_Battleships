654542efff4ff1546a4a2902e9099c2c859ba44e
Grenyerre <robertgrenyer@btinternet.com>
2024-05-21 12:12:51 +0100

    Add loop to create rows of board

--- run.py
+++ run.py
@@ -10,7 +10,10 @@ Legend:
 from random import randint
 from colorama import Fore, Back, Style
 
-# Construct the game board
+"""
+Construct the game boards via list comprehension
+to be populated with 'X' for placing ship and hit battleship
+"""
 hidden_board = [[''] * 8 for x in range(8)]
 guess_board = [[''] * 8 for x in range(8)]
 
@@ -22,7 +25,15 @@ letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H
 
 
 def print_board(board):
+    # Print column headings and border
+    print('  A B C D E F G H')
+    print('------------------')
 
+    # Print row number and borders
+    row_number = 1
+    for row in board:
+        print(str(row_number) + '|' + ' '.join(row) + '|')
+        row_number += 1
 
 def create_ships(board):
 
