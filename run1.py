c412544266562f6d9f82d8c90ef87dd211790ba8
Grenyerre <robertgrenyer@btinternet.com>
2024-05-21 11:36:56 +0100

    Import from random and colorama

--- run1.py
+++ run1.py
@@ -8,6 +8,7 @@ Legend:
 '-' for missed shot
 """
 from random import randint
+from colorama import Fore, Back, Style
 
 # Construct the game board
 hidden_board = [[''] * 8 for x in range(8)]
@@ -16,11 +17,11 @@ guess_board = [[''] * 8 for x in range(8)]
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 def print_board(board):
-    print('   A B C D E F G H')
-    print('--------------------')
+    print('  A B C D E F G H')
+    print('------------------')
     row_number = 1
     for row in board:
-        print(" %d|%s |" % (row_number, " |".join(row)))
+        print(str(row_number) + '|' + ' '.join(row) + '|')
         row_number += 1
 
 def create_ships(board):
