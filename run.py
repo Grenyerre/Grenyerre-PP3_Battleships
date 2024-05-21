9cae30c05ed3a8aac245b38ee49983abb288e876
Grenyerre <robertgrenyer@btinternet.com>
2024-05-21 11:56:38 +0100

    Add dictionary for letter to number conversion.

--- run.py
+++ run.py
@@ -13,3 +13,9 @@ from colorama import Fore, Back, Style
 # Construct the game board
 hidden_board = [[''] * 8 for x in range(8)]
 guess_board = [[''] * 8 for x in range(8)]
+
+"""
+Dictionary to convert letters to numbers allows user to use
+Integers for row and column input
+"""
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
